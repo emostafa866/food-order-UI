83c4ff86925a66b2fc24d5afa8f74d375c272678
youssefabdullah <youssefabdullah235@gmail.com>
2022-12-28 21:09:37 +0200

    Add service and handel logout and sign in

--- src/app/auth/auth-service.service.ts
+++ src/app/auth/auth-service.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, tap } from 'rxjs';
+import { UserLogin } from './user-login';
+import { UserRegister } from './user-register';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export  class AuthServiceService {
 
-  constructor() { }
+  private isLoggedIn = new BehaviorSubject<boolean>(false);//msh fahm deh ask Google
+  loggedIn = this.isLoggedIn.asObservable();//msh fahm
+
+  get token(){
+    return localStorage.getItem('auth')
+  }
+  constructor(private http: HttpClient) {
+    this.isLoggedIn.next(!!this.token)
+  }
+
+  public login(user: UserLogin) {
+
+    return this.http.post<UserLogin>('http://localhost:9091/api/auth/login', user).pipe(
+      tap(res => {
+        let token: any = res;
+        this.isLoggedIn.next(true)
+        localStorage.setItem('auth', token.jwt)
+      })
+    )
+
+  }
+  public register(user: UserRegister) {
+    return this.http.post<UserRegister>('http://localhost:9091/api/auth/register', user).pipe(
+      tap(res => {
+        let token: any = res;
+        this.isLoggedIn.next(true)
+        localStorage.setItem('auth', token.jwt)
+      })
+    );
+  }
 }
